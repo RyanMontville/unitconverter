2a2bcc4d52dd77ea7ee44c77e88505cbff0bfd71
Ryan Montville <ryan.a.montville@gmail.com>
2023-11-23 14:19:42 -0500

    first attempt to deploy app

--- Blazor/UnitConverter/UnitConverter/obj/Debug/net6.0/UnitConverter.AssemblyInfo.cs
+++ Blazor/UnitConverter/UnitConverter/obj/Debug/net6.0/UnitConverter.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnitConverter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00af42771fad8f0475c16891212d316600286ee3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnitConverter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnitConverter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

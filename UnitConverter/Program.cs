df2a6a157507b55e39e14aee892fed68131c3c6b
Ryan Montville <ryan.a.montville@gmail.com>
2024-06-12 11:46:03 -0500

    added history section

--- UnitConverter/Program.cs
+++ UnitConverter/Program.cs
@@ -1,26 +1,11 @@
-﻿using System;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using UnitConverter;
 
-namespace UnitConverter
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-            double tempC = ConvertToCelcius(100);
-            double tempF = ConvertToFerenheit(30);
-            Console.WriteLine(tempC);
-            Console.WriteLine(tempF);
-        }
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-        static double ConvertToCelcius(double ferenheit)
-        {
-            return Math.Round((ferenheit - 32) * 0.5556,2);
-        }
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-        static double ConvertToFerenheit(double celcius)
-        {
-            return Math.Round((celcius * 1.8) + 32);
-        }
-    }
-}
+await builder.Build().RunAsync();
